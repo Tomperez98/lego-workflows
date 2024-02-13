8c471a4ea72def47e7a93a618e78c3bc37dc23fd
Tomperez98 <tomasperezalvarez@gmail.com>
2024-02-13 14:11:30 -0500

    Initial layout

---
+++ lego_workflows/components.py
@@ -0,0 +1,41 @@
+"""Workflow definition components."""
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Generic, TypeVar
+
+if TYPE_CHECKING:
+    from result import Result
+
+
+@dataclass(frozen=True)
+class Response:
+    """Workflow response data."""
+
+
+@dataclass(frozen=True)
+class DomainEvent(ABC):
+    """Worflow event."""
+
+    @abstractmethod
+    async def publish(self) -> None:
+        """Publish event."""
+
+
+class DomainError(Exception):
+    """Raised when a user violates a business rule."""
+
+
+T = TypeVar("T")
+R = TypeVar("R", bound=Response)
+
+
+@dataclass(frozen=True)
+class Command(Generic[R, T]):
+    """Workflow input data."""
+
+    @abstractmethod
+    async def run(self, state_changes: list[T], events: list[DomainEvent]) -> R:
+        """Execute workflow."""
+        raise NotImplementedError
